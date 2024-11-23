801039ef7919321fb3f0b9f1c77ebd944965f407
bosch <130572668+boschaiyadej@users.noreply.github.com>
2024-11-23 18:20:27 +0700

    feat: add todo-list views

---
+++ src/components/Loading.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="flex items-center justify-center fixed inset-0 bg-black opacity-30"
+  >
+    <span class="loading loading-dots loading-lg"></span>
+  </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,21 @@
+import "./style.css";
+
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import App from "./App.vue";
 import router from "./router";
+
+library.add(faPenToSquare, faTrash);
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
