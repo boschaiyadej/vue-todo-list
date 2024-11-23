aa699de6e21f3d5b13bbc5b98dd61bd7c54e12d3
bosch <boschaiyadej@gmail.com>
2024-11-23 18:59:04 +0700

    feat: add edit page

--- src/main.js
+++ src/main.js
@@ -3,13 +3,17 @@ import "./style.css";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faPenToSquare, faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faPenToSquare,
+  faTrash,
+  faChevronLeft,
+} from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
 import App from "./App.vue";
 import router from "./router";
 
-library.add(faPenToSquare, faTrash);
+library.add(faPenToSquare, faTrash, faChevronLeft);
 
 const app = createApp(App);
 
